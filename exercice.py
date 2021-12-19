f31890a5859068732f613a91784b6ee40d19f22d
Muriellehxh <dmui49@gmail.com>
2021-12-19 11:16:23 -0500

    ch5 fini

--- exercice.py
+++ exercice.py
@@ -8,19 +8,40 @@ def get_bill(name, data):
 	INDEX_QUANTITY = 1
 	INDEX_PRICE = 2
 
-	return ""
+	list_sous_tot = []
+	for tup in data:
+		list_sous_tot.append(tup[2]*tup[1])
+
+	formated_sous_tot = format(sum(list_sous_tot), '.2f')
+	taxes = format(0.15 * float(formated_sous_tot), '.2f')
+	total = format(float(taxes) + float(formated_sous_tot), '.2f')
 
 
-def format_number(number, num_decimal_digits):
-	number = list(number)
-	print(number)
+	return f"{name}\nSOUS TOTAL{' '*(5)}{formated_sous_tot} $\n" \
+		   f"TAXES{' '* 10}{taxes} $\n" \
+		   f"TOTAL{' '* 10}{total} $"
+
+
+def format_number(number, num_decimal_digits): # 	REVOIR !!!
+	decimal_part = number % 1.0
+	print(decimal_part)
 
 	return []
 
 
 
 def get_triangle(num_rows):
-	return ""
+
+	n = 1
+	width = num_rows * 2 + 1
+
+	print('+' * width)
+	for row in range(0, num_rows):
+		A = 'A' * n
+		space = (width - 2 - n)//2
+		n+=2
+		print( f"+{' '*space}{A}{' '*space}+" )
+	print('+' * width)
 
 
 if __name__ == "__main__":
